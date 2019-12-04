dc1f7e43027cb9d5555842b6e16c513eec206a23
Francisco Gindre <francisco.gindre@gmail.com>
2019-12-04 17:56:53 -0300

    fix bitcode issue, add Progress HUD

---
+++ Example/ZcashLightClientSample/ZcashLightClientSample/List Transactions/TransactionsDataSource.swift
@@ -0,0 +1,9 @@
+//
+//  TransactionsDataSource.swift
+//  ZcashLightClientSample
+//
+//  Created by Francisco Gindre on 12/4/19.
+//  Copyright © 2019 Electric Coin Company. All rights reserved.
+//
+
+import Foundation
