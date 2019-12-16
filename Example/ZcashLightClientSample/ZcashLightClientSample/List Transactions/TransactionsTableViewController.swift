b88febf4ab7661bee981929f55c7faaf71d5f2b2
Francisco Gindre <francisco.gindre@gmail.com>
2019-12-16 19:13:06 -0300

    Transaction Detail screen

--- Example/ZcashLightClientSample/ZcashLightClientSample/List Transactions/TransactionsTableViewController.swift
+++ Example/ZcashLightClientSample/ZcashLightClientSample/List Transactions/TransactionsTableViewController.swift
@@ -7,7 +7,7 @@
 //
 
 import UIKit
-
+import ZcashLightClientKit
 class TransactionsTableViewController: UITableViewController {
     
     var datasource: TransactionsDataSource? {
@@ -20,12 +20,13 @@ class TransactionsTableViewController: UITableViewController {
         }
     }
     
+    var selectedTx: TransactionEntity?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.tableView.dataSource = datasource
         datasource?.load()
         self.tableView.reloadData()
-        
     }
 
     // MARK: - Table view data source
@@ -39,5 +40,36 @@ class TransactionsTableViewController: UITableViewController {
         // #warning Incomplete implementation, return the number of rows
         return 0
     }
+    
+    var selectedRow: Int?
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        selectedRow = indexPath.row
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        if shouldPerformSegue(withIdentifier: "TransactionDetail", sender: self) {
+            performSegue(withIdentifier: "TransactionDetail", sender: self)
+        }
+    }
+    
+    override func shouldPerformSegue(withIdentifier identifier: String, sender: Any?) -> Bool {
+        if identifier == "TransactionDetail",
+            let row = selectedRow,
+            let tx = datasource?.transactions[row] {
+             selectedTx = tx
+            return true
+            
+        }
+        return false
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let destination = segue.destination as? TransactionDetailViewController, let tx = selectedTx {
+            destination.model = tx
+            selectedTx = nil
+            selectedRow = nil
+        }
+    }
 
 }
+ 
---
+++ Example/ZcashLightClientSample/ZcashLightClientSample/Transaction Detail/TransactionDetailViewController.swift
@@ -0,0 +1,117 @@
+//
+//  TransactionDetailViewController.swift
+//  ZcashLightClientSample
+//
+//  Created by Francisco Gindre on 12/16/19.
+//  Copyright © 2019 Electric Coin Company. All rights reserved.
+//
+
+import UIKit
+import ZcashLightClientKit
+class TransactionDetailViewController: UITableViewController {
+    
+    @IBOutlet weak var idLabel: UILabel!
+    @IBOutlet weak var minedHeightLabel: UILabel!
+    @IBOutlet weak var expiryHeightLabel: UILabel!
+    @IBOutlet weak var createdLabel: UILabel!
+    
+    var model: TransactionEntity!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        setup()
+    }
+    
+    func setup() {
+        guard model != nil else { return }
+        idLabel.text = model.transactionId.hexEncodedString()
+        minedHeightLabel.text = heightToString(height: model.minedHeight)
+        expiryHeightLabel.text = heightToString(height: model.expiryHeight)
+        createdLabel.text = model.created
+    }
+    
+    
+    func formatMemo(_ memo: Data?) -> String {
+        guard let memo = memo, let string = String(bytes: memo, encoding: .utf8) else { return "No Memo" }
+        return string
+    }
+    
+    
+    func heightToString(height: BlockHeight?) -> String {
+        guard let height = height else { return "NULL" }
+        return String(height)
+    }
+    // MARK: - Table view data source
+//
+//    override func numberOfSections(in tableView: UITableView) -> Int {
+//        // #warning Incomplete implementation, return the number of sections
+//        return 0
+//    }
+//
+//    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        // #warning Incomplete implementation, return the number of rows
+//        return 0
+//    }
+
+    /*
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+
+        // Configure the cell...
+
+        return cell
+    }
+    */
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
