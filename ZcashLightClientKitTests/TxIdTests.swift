8bb5eca9a6403b4067153c11ddaa72beae5fb1f8
Francisco Gindre <francisco.gindre@gmail.com>
2020-01-15 15:30:59 -0300

    fix testTxIdAsString test

--- ZcashLightClientKitTests/TxIdTests.swift
+++ ZcashLightClientKitTests/TxIdTests.swift
@@ -11,7 +11,7 @@ class TxIdTests: XCTestCase {
 
     func testTxIdAsString() {
         let transactionId = "5cf915c5d01007c39d602e08ab59d98aba366e2fb7ac01f2cdad4bf4f8f300bb"
-        let expectedTxIdString = "bb003f8f4fb4dadc2f10ca7bf2e663aba89d95ba80e206d93c70010d5c519fc5"
+        let expectedTxIdString = "bb00f3f8f44badcdf201acb72f6e36ba8ad959ab082e609dc30710d0c515f95c"
         
         XCTAssertEqual(transactionId.hexDecodedData().toHexStringTxId(), expectedTxIdString)
     }
