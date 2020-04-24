0bcbdeeba0c6e4753d862f8a7d0ddacf71d52087
Francisco Gindre <francisco.gindre@gmail.com>
2020-04-24 14:48:11 -0300

    Memo Tests

--- ZcashLightClientKit/Extensions/Data+Zcash.swift
+++ ZcashLightClientKit/Extensions/Data+Zcash.swift
@@ -14,29 +14,5 @@ public extension Data {
     func toHexStringTxId() -> String {
         self.hexEncodedString().toTxIdString()
     }
-    
-}
-extension String {
-    func toTxIdString() -> String {
-        var id = ""
-        self.reversed().pairs.map {
-            $0.reversed()
-        }.forEach { (reversed) in
-            id.append(String(reversed))
-        }
-        return id
-    }
-}
 
-extension Collection {
-    var pairs: [SubSequence] {
-        var startIndex = self.startIndex
-        let count = self.count
-        let n = count / 2 + count % 2
-        return (0..<n).map { _ in
-            let endIndex = index(startIndex, offsetBy: 2, limitedBy: self.endIndex) ?? self.endIndex
-            defer { startIndex = endIndex }
-            return self[startIndex..<endIndex]
-        }
-    }
 }
--- ZcashLightClientKitTests/MemoTests.swift
+++ ZcashLightClientKitTests/MemoTests.swift
@@ -6,50 +6,83 @@
 //
 
 import XCTest
-
+@testable import ZcashLightClientKit
 class MemoTests: XCTestCase {
-
+    
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
-
+    
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
     
     /**
-      Non-utf8 memos are properly ignored
+     Non-utf8 memos are properly ignored
      */
     func testNonUnicodeMemos() throws {
-        
+        XCTAssertNil(Self.randomMemoData()!.asZcashTransactionMemo())
     }
     
     /**
      Memo length is correct, padding characters are ignored
      */
     func testMemoLength() throws {
-        
+        XCTAssertEqual(validMemoData.count, 512)
+        XCTAssertEqual(validMemoData.asZcashTransactionMemo()!.count, Self.validMemoDataExpectedString.count)
     }
     /**
      Verify support for common unicode characters
      */
     func testUnicodeCharacters() throws {
         
+        let memo = validMemoData.asZcashTransactionMemo()
+        XCTAssertNotNil(memo)
+        XCTAssertEqual(memo!, Self.validMemoDataExpectedString)
+        
     }
-
+    
     /**
      Blank memos are ignored
      */
     func testBlankMemos() throws {
         // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        XCTAssertNil(emptyMemoData.asZcashTransactionMemo())
     }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    
+    /**
+     *******
+     * mocked memos
+     * ******
+     */
+    
+    /**
+     Real text:  "Here's gift from the Zec Fairy @ ECC!"
+     */
+    static let validMemoDataExpectedString = "Here's gift from the Zec Fairy @ ECC!"
+    
+    static let validMemoDataBase64 = "SGVyZSdzIGdpZnQgZnJvbSB0aGUgWmVjIEZhaXJ5IEAgRUNDIQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA="
+    
+    let validMemoData = Data(base64Encoded: validMemoDataBase64)!
+    
+    let emptyMemoData = Data([UInt8](repeating: 0, count: 512))
+    
+    let totallyRandomDataMemo = randomMemoData()!
+    
+    
+    
+    static func randomMemoData() -> Data? {
+        let length: Int = 512
+        var keyData = Data(count: length)
+        let result = keyData.withUnsafeMutableBytes {
+            SecRandomCopyBytes(kSecRandomDefault, length, $0.baseAddress!)
+        }
+        if result == errSecSuccess {
+            return keyData
+        } else {
+            print("Problem generating random bytes")
+            return nil
         }
     }
-
 }
+
