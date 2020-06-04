f5fdb027ff8e4b417e05444fc1893fab1ec148ab
Francisco Gindre <francisco.gindre@gmail.com>
2020-06-04 18:34:43 -0300

    Fixes isu 136 - expiry height -1 on pending transactions

--- ZcashLightClientKit/Transaction/WalletTransactionEncoder.swift
+++ ZcashLightClientKit/Transaction/WalletTransactionEncoder.swift
@@ -11,12 +11,11 @@ class WalletTransactionEncoder: TransactionEncoder {
     
     var rustBackend: ZcashRustBackendWelding.Type
     var repository: TransactionRepository
-//    var initializer: Initializer
     var queue: DispatchQueue
     private var outputParamsURL: URL
     private var spendParamsURL: URL
     private var dataDbURL: URL
-
+    
     init(rust: ZcashRustBackendWelding.Type,
          dataDb: URL,
          repository: TransactionRepository,
@@ -75,7 +74,7 @@ class WalletTransactionEncoder: TransactionEncoder {
         guard ensureParams(spend: self.spendParamsURL, output: self.spendParamsURL) else {
             throw TransactionEncoderError.missingParams
         }
-                
+        
         let txId = rustBackend.createToAddress(dbData: self.dataDbURL, account: Int32(accountIndex), extsk: spendingKey, to: address, value: Int64(zatoshi), memo: memo, spendParamsPath: self.spendParamsURL.path, outputParamsPath: self.outputParamsURL.path)
         
         guard txId > 0 else {
@@ -91,5 +90,18 @@ class WalletTransactionEncoder: TransactionEncoder {
         let readableOutput = FileManager.default.isReadableFile(atPath: output.path)
         
         return readableSpend && readableOutput // Todo: change this to something that makes sense
+    }
+    
+    /**
+     Fetch the Transaction Entity from the encoded representation
+     - Parameter encodedTransaction: The encoded transaction to expand
+     - Returns: a TransactionEntity based on the given Encoded Transaction
+     - Throws: a TransactionEncoderError
+     */
+    func expandEncodedTransaction(_ encodedTransaction: EncodedTransaction) throws -> TransactionEntity {
+        guard let t = try? repository.findBy(rawId: encodedTransaction.transactionId) else {
+            throw TransactionEncoderError.couldNotExpand(txId: encodedTransaction.transactionId)
+        }
+        return t
     }
 }
