14a5a02966f05c2748a08f622bfd2a55f0db2284
Francisco Gindre <francisco.gindre@gmail.com>
2021-03-15 10:52:53 -0300

    Logger fixes

--- Example/ZcashLightClientSample/ZcashLightClientSample/SampleLogger.swift
+++ Example/ZcashLightClientSample/ZcashLightClientSample/SampleLogger.swift
@@ -19,44 +19,54 @@ class SampleLogger: ZcashLightClientKit.Logger {
         case info
     }
     
+    enum LoggerType {
+        case osLog
+        case printerLog
+    }
+    
     var level: LogLevel
-    init(logLevel: LogLevel) {
+    var loggerType: LoggerType
+    
+    init(logLevel: LogLevel, type: LoggerType = .osLog) {
         self.level = logLevel
+        self.loggerType = type
     }
     
     private static let subsystem = Bundle.main.bundleIdentifier!
-    static let oslog = OSLog(subsystem: subsystem, category: "sample-logs")
+    static let oslog = OSLog(subsystem: subsystem, category: "logs")
     
-    func debug(_ message: String, file: String = #file, function: String = #function, line: Int = #line) {
+    func debug(_ message: String, file: StaticString = #file, function: StaticString = #function, line: Int = #line) {
         guard level.rawValue == LogLevel.debug.rawValue else { return }
         log(level: "DEBUG 🐞", message: message, file: file, function: function, line: line)
     }
     
-    func error(_ message: String, file: String = #file, function: String = #function, line: Int = #line) {
+    func error(_ message: String, file: StaticString = #file, function: StaticString = #function, line: Int = #line) {
         guard level.rawValue <= LogLevel.error.rawValue else { return }
         log(level: "ERROR 💥", message: message, file: file, function: function, line: line)
     }
     
-    func warn(_ message: String, file: String = #file, function: String = #function, line: Int = #line) {
+    func warn(_ message: String, file: StaticString = #file, function: StaticString = #function, line: Int = #line) {
            guard level.rawValue <= LogLevel.warning.rawValue else { return }
            log(level: "WARNING ⚠️", message: message, file: file, function: function, line: line)
     }
 
-    func event(_ message: String, file: String = #file, function: String = #function, line: Int = #line) {
+    func event(_ message: String, file: StaticString = #file, function: StaticString = #function, line: Int = #line) {
         guard level.rawValue <= LogLevel.event.rawValue else { return }
         log(level: "EVENT ⏱", message: message, file: file, function: function, line: line)
     }
     
-    func info(_ message: String, file: String = #file, function: String = #function, line: Int = #line) {
+    func info(_ message: String, file: StaticString = #file, function: StaticString = #function, line: Int = #line) {
         guard level.rawValue <= LogLevel.info.rawValue else { return }
         log(level: "INFO ℹ️", message: message, file: file, function: function, line: line)
     }
     
-    private func log(level: String, message: String, file: String, function: String, line: Int) {
-        let fileName = file as NSString
-        
-        os_log("[%@] %@ - %@ - Line: %d -> %@", log: Self.oslog, type: .default, level, fileName.lastPathComponent, function, line, message)
+    private func log(level: String, message: String, file: StaticString = #file, function: StaticString = #function, line: Int = #line) {
+        let fileName = (String(describing: file) as NSString).lastPathComponent
+        switch loggerType {
+        case .printerLog:
+            print("[\(level)] \(fileName) - \(function) - line: \(line) -> \(message)")
+        default:
+            os_log("[%{public}@] %{public}@ - %{public}@ - Line: %{public}d -> %{public}@", level, fileName, String(describing: function), line, message)
+        }
     }
-    
-    
 }
