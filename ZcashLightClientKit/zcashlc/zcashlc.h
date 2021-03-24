946a8a764e4ef5ccda6f58ce3b1b2471f07fa585
Charlie O'Keefe <git@charlie.okeefe.name>
2021-03-24 11:53:46 -0600

    commit removal of zcashlc_is_valid_viewing_key

--- ZcashLightClientKit/zcashlc/zcashlc.h
+++ ZcashLightClientKit/zcashlc/zcashlc.h
@@ -189,8 +189,6 @@ bool zcashlc_is_valid_shielded_address(const char *address);
  */
 bool zcashlc_is_valid_transparent_address(const char *address);
 
-bool zcashlc_is_valid_viewing_key(const char *key);
-
 /**
  * returns whether the given viewing key is valid or not
  */
