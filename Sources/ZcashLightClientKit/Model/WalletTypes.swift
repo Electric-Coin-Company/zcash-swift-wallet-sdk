720f9bea86c7634adca2fa5112bb1836e67b8490
Daniel Haight <other+ecc@haight.io>
2022-03-20 00:11:32 +0000

    Update wallet birthday methods to use bundled checkpoints

--- Sources/ZcashLightClientKit/Model/WalletTypes.swift
+++ Sources/ZcashLightClientKit/Model/WalletTypes.swift
@@ -32,6 +32,56 @@ public struct WalletBirthday {
     public private(set) var tree: String = ""
 }
 
+
+extension WalletBirthday: Decodable {
+  //  let height: BlockHeight
+  //  let hash: String
+  //  let time: UInt32
+  //  let tree: String
+  //
+  //  func walletBirthday() -> WalletBirthday {
+  //    WalletBirthday(
+  //      height: height,
+  //      hash: hash,
+  //      time: time,
+  //      tree: tree
+  //    )
+  //  }
+
+  public enum CodingKeys: String, CodingKey {
+    case height
+    case hash
+    case time
+    case tree
+  }
+
+  public init(from decoder: Decoder) throws {
+    let container = try decoder.container(keyedBy: CodingKeys.self)
+    self.height = try Self.getHeight(from: container)
+    self.hash = try container.decode(String.self, forKey: .hash)
+    self.time = try container.decode(UInt32.self, forKey: .time)
+    self.tree = try container.decode(String.self, forKey: .tree)
+  }
+
+  static func getHeight(from container: KeyedDecodingContainer<CodingKeys>) throws -> Int {
+    guard
+      let heightString = try? container.decode(String.self, forKey: .height),
+      let height = Int(heightString)
+    else {
+      throw DecodingError.typeMismatch(
+        String.self,
+        DecodingError.Context(
+          codingPath: [CodingKeys.height],
+          debugDescription: "expected height to be encoded as a string",
+          underlyingError: nil
+        )
+      )
+    }
+    return height
+  }
+}
+
+
 /**
 Groups a Sapling Extended Full Viewing Key an a tranparent address extended public key.
 */
