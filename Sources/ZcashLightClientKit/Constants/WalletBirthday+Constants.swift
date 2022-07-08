9f15ce392b8b59bdaaa039fa62099390e674e8de
Francisco Gindre <francisco.gindre@gmail.com>
2022-07-08 18:49:54 -0300

    [#406] some BirthdayTests fail for MacOS target
    Closes #406
    
    This commits differences how Checkpoints are retrieved from the resource bundle
    depending on the target platform.

--- Sources/ZcashLightClientKit/Constants/WalletBirthday+Constants.swift
+++ Sources/ZcashLightClientKit/Constants/WalletBirthday+Constants.swift
@@ -8,51 +8,106 @@
 import Foundation
 
 public extension WalletBirthday {
-  static func birthday(with height: BlockHeight, network: ZcashNetwork) -> WalletBirthday {
-    switch network.networkType {
-    case .mainnet:
-      return birthday(with: height, checkpointDirectory: Self.mainnetCheckpointDirectory) ?? .mainnetMin
-    case .testnet:
-      return birthday(with: height, checkpointDirectory: Self.testnetCheckpointDirectory) ?? .testnetMin
+    static func birthday(with height: BlockHeight, network: ZcashNetwork) -> WalletBirthday {
+        let checkpointDirectoryURL = BundleCheckpointURLProvider.default.url(network.networkType)
+
+        switch network.networkType {
+        case .mainnet:
+            return birthday(with: height, checkpointDirectory: checkpointDirectoryURL) ?? .mainnetMin
+        case .testnet:
+            return birthday(with: height, checkpointDirectory: checkpointDirectoryURL) ?? .testnetMin
+        }
     }
-  }
 }
 
 extension WalletBirthday {
-  static func birthday(with height: BlockHeight, checkpointDirectory: URL) -> WalletBirthday? {
-    return bestCheckpointHeight(for: height, checkpointDirectory: checkpointDirectory)
-      .flatMap { checkpoint(height: $0, directory: checkpointDirectory) }
-  }
-
-  private static func bestCheckpointHeight(for height: BlockHeight, checkpointDirectory: URL) -> Int? {
-    guard let checkPointURLs = try? FileManager.default.contentsOfDirectory(
-      at: checkpointDirectory,
-      includingPropertiesForKeys: nil,
-      options: .skipsHiddenFiles
-    ) else {
-      return nil
+    static func birthday(with height: BlockHeight, checkpointDirectory: URL) -> WalletBirthday? {
+        return bestCheckpointHeight(for: height, checkpointDirectory: checkpointDirectory)
+            .flatMap { checkpoint(height: $0, directory: checkpointDirectory) }
     }
 
-    return checkPointURLs
-      .map { $0.deletingPathExtension() }
-      .map(\.lastPathComponent)
-      .compactMap(Int.init)
-      .filter { $0 <= height }
-      .sorted()
-      .last
-  }
+    private static func bestCheckpointHeight(for height: BlockHeight, checkpointDirectory: URL) -> Int? {
+        guard let checkPointURLs = try? FileManager.default.contentsOfDirectory(
+            at: checkpointDirectory,
+            includingPropertiesForKeys: nil,
+            options: .skipsHiddenFiles
+        ) else {
+            return nil
+        }
 
-  private static func checkpoint(height: BlockHeight, directory checkpointDirectory: URL) -> WalletBirthday? {
-    let url = checkpointDirectory
-      .appendingPathComponent(String(height))
-      .appendingPathExtension("json")
+        return checkPointURLs
+            .map { $0.deletingPathExtension() }
+            .map(\.lastPathComponent)
+            .compactMap(Int.init)
+            .filter { $0 <= height }
+            .sorted()
+            .last
+    }
 
-    return try? checkpoint(at: url)
-  }
+    private static func checkpoint(height: BlockHeight, directory checkpointDirectory: URL) -> WalletBirthday? {
+        let url = checkpointDirectory
+            .appendingPathComponent(String(height))
+            .appendingPathExtension("json")
 
-  private static func checkpoint(at url: URL) throws -> WalletBirthday {
-    let data = try Data(contentsOf: url)
-    let checkpoint = try JSONDecoder().decode(WalletBirthday.self, from: data)
-    return checkpoint
-  }
+        return try? checkpoint(at: url)
+    }
+
+    private static func checkpoint(at url: URL) throws -> WalletBirthday {
+        let data = try Data(contentsOf: url)
+        let checkpoint = try JSONDecoder().decode(WalletBirthday.self, from: data)
+        return checkpoint
+    }
+}
+
+struct BundleCheckpointURLProvider {
+    var url: (NetworkType) -> URL
+}
+
+
+extension BundleCheckpointURLProvider {
+
+    /// Attempts to resolve the platform by checking `#if os(macOS)` build corresponds to a MacOS target
+    /// `#else` branch of that condition will assume iOS is the target platform
+    static var `default` = BundleCheckpointURLProvider { networkType in
+        #if os(macOS)
+        Self.macOS.url(networkType)
+        #else
+        Self.iOS.url(networkType)
+        #endif
+    }
+
+    static var iOS = BundleCheckpointURLProvider(url: { networkType in
+        switch networkType {
+        case .mainnet:
+            return WalletBirthday.mainnetCheckpointDirectory
+        case .testnet:
+            return WalletBirthday.testnetCheckpointDirectory
+        }
+    })
+
+    /// This variant attempts to retrieve the saplingActivation checkpoint for the given network type
+    /// using `Bundle.module.url(forResource:withExtension:subdirectory:localization)`
+    /// if not found it will return `WalletBirthday.mainnetCheckpointDirectory` or
+    /// `WalletBirthday.testnetCheckpointDirectory`. This responds to tests
+    /// failing on MacOS target because the checkpoint resources would fail.
+    static var macOS = BundleCheckpointURLProvider(url: { networkType in
+        switch networkType {
+        case .mainnet:
+            return Bundle.module.url(
+                forResource: "419200",
+                withExtension: "json",
+                subdirectory: "checkpoints/mainnet/",
+                localization: nil
+            )?
+            .deletingLastPathComponent() ?? WalletBirthday.mainnetCheckpointDirectory
+        case .testnet:
+           return Bundle.module.url(
+                forResource: "280000",
+                withExtension: "json",
+                subdirectory: "checkpoints/testnet/",
+                localization: nil
+            )?
+            .deletingLastPathComponent() ?? WalletBirthday.testnetCheckpointDirectory
+        }
+    })
 }
