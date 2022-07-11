b6bb17ab8beccc61a8eee90e09a04ecf34fe7bde
Francisco Gindre <francisco@z.cash>
2022-07-11 09:17:22 -0300

    [#406] some BirthdayTests fail for MacOS target (#410)
    Closes #406
    
    This commits differences how Checkpoints are retrieved from the resource bundle
    depending on the target platform.

--- Sources/ZcashLightClientKit/Constants/WalletBirthday+mainnet.swift
+++ Sources/ZcashLightClientKit/Constants/WalletBirthday+mainnet.swift
@@ -7,13 +7,12 @@
 import Foundation
 
 extension WalletBirthday {
-  static let mainnetMin = WalletBirthday(
-    height: 419_200,
-    hash: "00000000025a57200d898ac7f21e26bf29028bbe96ec46e05b2c17cc9db9e4f3",
-    time: 1540779337,
-    saplingTree: "000000"
-  )
+    static let mainnetMin = WalletBirthday(
+        height: 419_200,
+        hash: "00000000025a57200d898ac7f21e26bf29028bbe96ec46e05b2c17cc9db9e4f3",
+        time: 1540779337,
+        saplingTree: "000000"
+    )
 
-  static let mainnetCheckpointDirectory = Bundle.module.bundleURL.appendingPathComponent("checkpoints/mainnet/")
-
+    static let mainnetCheckpointDirectory = Bundle.module.bundleURL.appendingPathComponent("checkpoints/mainnet/")
 }
