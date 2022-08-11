1237854ef87f8f8ab724a76d548064a5e941fd67
pacu <pacu@pacus-MacBook-Pro.local>
2022-08-11 12:10:32 -0700

    [#455] revert queue priority downgrade changes from [#435]
    Closes #455
    
    this reverts queue priority changes from commit `a5d0e447748257d2af5c9101391dd05a5ce929a2` since we detected it might prevent downloads to be scheduled in a
    timely fashion

--- Sources/ZcashLightClientKit/Block/DatabaseStorage/CompactBlockStorage.swift
+++ Sources/ZcashLightClientKit/Block/DatabaseStorage/CompactBlockStorage.swift
@@ -86,7 +86,7 @@ extension CompactBlockStorage: CompactBlockRepository {
     }
     
     func latestHeight(result: @escaping (Swift.Result<BlockHeight, Error>) -> Void) {
-        DispatchQueue.global(qos: .default).async {
+        DispatchQueue.global(qos: .userInitiated).async {
             do {
                 result(.success(try self.latestBlockHeight()))
             } catch {
@@ -100,7 +100,7 @@ extension CompactBlockStorage: CompactBlockRepository {
     }
     
     func write(blocks: [ZcashCompactBlock], completion: ((Error?) -> Void)?) {
-        DispatchQueue.global(qos: .default).async {
+        DispatchQueue.global(qos: .userInitiated).async {
             do {
                 try self.insert(blocks)
                 completion?(nil)
@@ -111,7 +111,7 @@ extension CompactBlockStorage: CompactBlockRepository {
     }
     
     func rewind(to height: BlockHeight, completion: ((Error?) -> Void)?) {
-        DispatchQueue.global(qos: .default).async {
+        DispatchQueue.global(qos: .userInitiated).async {
             do {
                 try self.rewind(to: height)
                 completion?(nil)
