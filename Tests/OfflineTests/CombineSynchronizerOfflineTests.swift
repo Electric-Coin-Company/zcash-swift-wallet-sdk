63b6bd91d99ddfe9f1c495cee46fcc7b92ad7ac2
Michal Fousek <chlup@chlup.info>
2023-03-20 16:34:51 +0100

    [#831] Add tests for ClosureSDKSynchronizer
    - Added tests are testing in general if the `ClosuresSDKSynchronizer` is correctly
      calling `Synchronizer` and if the values are correctly returned.
    - `ClosuresSDKSynchronizer` doesn't contain any logic but it is public
      API and we should be sure that it works correctly.

---
+++ Tests/OfflineTests/CombineSynchronizerOfflineTests.swift
@@ -0,0 +1,16 @@
+//
+//  CombineSynchronizerOfflineTests.swift
+//  
+//
+//  Created by Michal Fousek on 20.03.2023.
+//
+
+import Combine
+import Foundation
+import XCTest
+@testable import ZcashLightClientKit
+
+class CombineSynchronizerOfflineTests: XCTestCase {
+
+}
+
