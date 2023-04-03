16d70cfead166ae214bc813fd245d3eb62588cfd
Lukas Korba <LukasKorba@users.noreply.github.com>
2023-04-03 09:16:07 +0200

    [#896] Fix SDK compilation fail caused by Swift 5.8 (#897)
    - Fixed the guard with explicit self

--- Sources/ZcashLightClientKit/Block/CompactBlockProcessor.swift
+++ Sources/ZcashLightClientKit/Block/CompactBlockProcessor.swift
@@ -1109,7 +1109,7 @@ actor CompactBlockProcessor {
             repeats: true,
             block: { [weak self] _ in
                 Task { [self] in
-                    guard let self else { return }
+                    guard let self = self else { return }
                     if await self.shouldStart {
                         self.logger.debug(
                             """
