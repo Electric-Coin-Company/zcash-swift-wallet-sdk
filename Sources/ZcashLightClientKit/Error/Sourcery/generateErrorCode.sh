b9182b65fda89bf07aa973b1d5a5766e2aedb997
Michal Fousek <chlup@chlup.info>
2023-04-12 15:07:40 +0200

    Fix script which generates ZcashError

--- Sources/ZcashLightClientKit/Error/Sourcery/generateErrorCode.sh
+++ Sources/ZcashLightClientKit/Error/Sourcery/generateErrorCode.sh
@@ -8,6 +8,7 @@ sourcery_version=2.0.2
 if which sourcery >/dev/null; then
     if [[ $(sourcery --version) != $sourcery_version ]]; then
         echo "warning: Compatible sourcer version not installed. Install sourcer $sourcery_version. Currently installed version is $(sourcer --version)"
+        exit 1
     fi
 
     sourcery \
