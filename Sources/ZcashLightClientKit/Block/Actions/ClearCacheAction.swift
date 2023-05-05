7f3d8dd7574d96d9f7fd0d27d32f1b660e6afac0
Michal Fousek <chlup@chlup.info>
2023-05-05 17:04:13 +0200

    [#700] CompactBlockProcessor as state machine proof of concept
    - For now I created `CompactBlockProcessorNG` where I started with
      implementation of the state machine. I did it to not break the rest of
      the SDK. This change will be merged to the feature branch. And before
      it is merged to `main` branch code from `CompactBlockProcessorNG` will
      be moved to `CompactBlockProcessor`.
    - The new code is not used. It just shows and explains how it is done.
      It is proof of concept.
    - I did put either commented current code or comment to some places to
      explain what should be done there.
    - New important data types:
      - `ActionContext` is context that can hold any data that needs to be
        shared between actions. For example sync ranges or current state.
      - `CBPState` is state of the `CompactBlockProcessor`. Each state is
        handled by one action. This doesn't apply to terminal states like
        `finished` or `failed`.
      - `ActionProgress` is very similar to `CompactBlockProgress`.
        Different actions reports progress differently and `ActionProgress`
        represents this.
      - `Action` is protocol that defines API of an action. It has one run
        method that executes the code of the action
    - CBP first creates actions for (almost) each state in `makeActions()`
      method. Then the "magic" is done in `CompactBlockProcessorNG.run()` method.
      Here is main loop which takes action for current state and execute it.
      It's expected that action does it's work and then updates the context
      with new state. And this happens until some terminal state
      (`finished`, `failed`, `stopped`) is reached.
    - After the transition to state machine API of the
      `CompactBlockProcessor` should stay the same. No changes should be
      required in `SDKSynchronizer`.

---
+++ Sources/ZcashLightClientKit/Block/Actions/ClearCacheAction.swift
@@ -0,0 +1,20 @@
+//
+//  ClearCacheAction.swift
+//  
+//
+//  Created by Michal Fousek on 05.05.2023.
+//
+
+import Foundation
+
+class ClearCacheAction {
+    init() { }
+}
+
+extension ClearCacheAction: Action {
+    func run(with context: ActionContext, didUpdate: @escaping (ActionProgress) async -> Void) async throws -> ActionContext {
+        // clear storage
+        await context.update(state: .finished)
+        return context
+    }
+}
