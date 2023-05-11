ce8fcdf3cc4207ebbfb63f1eed716b001b6fcd9c
Lukas Korba <lukas.korba@seznam.cz>
2023-05-11 12:48:45 +0200

    [#1045] Implement FetchUTXOsAction
    - draft of the fetching
    
    [#1045] Implement FetchUTXOsAction
    
    - updated the way Actions communicate data back to the CBP
    - used this mechanism to pass result of utxos fetch so it's passed to the SDKSynchronizer as an Event

--- Sources/ZcashLightClientKit/Block/Actions/ComputeSyncRangesAction.swift
+++ Sources/ZcashLightClientKit/Block/Actions/ComputeSyncRangesAction.swift
@@ -12,7 +12,7 @@ class ComputeSyncRangesAction {
 }
 
 extension ComputeSyncRangesAction: Action {
-    func run(with context: ActionContext, didUpdate: @escaping (CompactBlockProgress) async -> Void) async throws -> ActionContext {
+    func run(with context: ActionContext, didUpdate: @escaping (CompactBlockProcessorNG.Event) async -> Void) async throws -> ActionContext {
         // call internalSyncProgress and compute sync ranges and store them in context
         // if there is nothing sync just switch to finished state
 
