07bb21d0a420b04f86f573c4f52394138af6c041
Michal Fousek <chlup@chlup.info>
2023-05-12 11:49:15 +0200

    [#1044] Implement EnhanceAction
    Closes #1044

--- Sources/ZcashLightClientKit/Block/Actions/EnhanceAction.swift
+++ Sources/ZcashLightClientKit/Block/Actions/EnhanceAction.swift
@@ -8,7 +8,33 @@
 import Foundation
 
 class EnhanceAction {
-    init(container: DIContainer) { }
+    let blockEnhancer: BlockEnhancer
+    let config: CompactBlockProcessorNG.Configuration
+    let internalSyncProgress: InternalSyncProgress
+    let logger: Logger
+    let transactionRepository: TransactionRepository
+    init(container: DIContainer, config: CompactBlockProcessorNG.Configuration) {
+        blockEnhancer = container.resolve(BlockEnhancer.self)
+        self.config = config
+        internalSyncProgress = container.resolve(InternalSyncProgress.self)
+        logger = container.resolve(Logger.self)
+        transactionRepository = container.resolve(TransactionRepository.self)
+    }
+
+    func decideWhatToDoNext(context: ActionContext, lastScannedHeight: BlockHeight) async -> ActionContext {
+        guard let downloadAndScanRange = await context.syncRanges.downloadAndScanRange else {
+            await context.update(state: .clearCache)
+            return context
+        }
+
+        if lastScannedHeight >= downloadAndScanRange.upperBound {
+            await context.update(state: .clearCache)
+        } else {
+            await context.update(state: .download)
+        }
+
+        return context
+    }
 }
 
 extension EnhanceAction: Action {
@@ -23,8 +49,25 @@ extension EnhanceAction: Action {
         // If latestScannedHeight < context.scanRanges.downloadAndScanRange?.upperBound thne set state to `download` because there are blocks to
         // download and scan.
 
-        await context.update(state: .clearCache)
-        return context
+        let lastScannedHeight = try await transactionRepository.lastScannedHeight()
+
+        guard let range = await context.syncRanges.enhanceRange else {
+            return await decideWhatToDoNext(context: context, lastScannedHeight: lastScannedHeight)
+        }
+
+        let lastEnhancedHeight = await internalSyncProgress.load(.latestEnhancedHeight)
+        let enhanceRangeStart = max(range.lowerBound, lastEnhancedHeight)
+        let enhanceRangeEnd = min(range.upperBound, lastScannedHeight)
+
+        if enhanceRangeStart <= enhanceRangeEnd && lastEnhancedHeight - lastScannedHeight >= config.enhanceBatchSize {
+            let enhanceRange = enhanceRangeStart...enhanceRangeEnd
+            let transactions = try await blockEnhancer.enhance(at: enhanceRange) { progress in
+                await didUpdate(.progressUpdated(.enhance(progress)))
+            }
+            await didUpdate(.foundTransactions(transactions, enhanceRange))
+        }
+
+        return await decideWhatToDoNext(context: context, lastScannedHeight: lastScannedHeight)
     }
 
     func stop() async { }
