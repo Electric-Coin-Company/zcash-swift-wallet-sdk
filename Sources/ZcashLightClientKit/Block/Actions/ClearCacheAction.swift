db7f98d438c77210e784a26cb99374b6552fa1ed
Lukas Korba <lukas.korba@seznam.cz>
2023-08-09 10:03:36 +0200

    [#1188] Working prototype of SbS
    - cleaned up the code
    - ScanAlgorithm enum added to the SDK
    - preferred sync algorithm set to .linear as default but can be changed to Spend before Sync as the Initializer.init parameter
    
    [#1188] Working prototype of SbS
    
    - error codes for failure states in the SbS State Machine changes added
    
    [#1188] Working prototype of SbS (#1192)
    
    - offline tests fixed

--- Sources/ZcashLightClientKit/Block/Actions/ClearCacheAction.swift
+++ Sources/ZcashLightClientKit/Block/Actions/ClearCacheAction.swift
@@ -23,8 +23,19 @@ extension ClearCacheAction: Action {
         if await context.prevState == .idle {
             await context.update(state: .migrateLegacyCacheDB)
         } else {
-            //await context.update(state: .finished) // Linear
-            await context.update(state: .validatePreviousWalletSession)
+            if context.preferredSyncAlgorithm == .linear {
+                await context.update(state: .finished)
+            } else {
+                if let supportedSyncAlgorithm = await context.supportedSyncAlgorithm {
+                    if supportedSyncAlgorithm == .linear {
+                        await context.update(state: .finished)
+                    } else {
+                        await context.update(state: .processSuggestedScanRanges)
+                    }
+                } else {
+                    throw ZcashError.compactBlockProcessorSupportedSyncAlgorithm
+                }
+            }
         }
         return context
     }
---
+++ Sources/ZcashLightClientKit/Block/Actions/ProcessSuggestedScanRangesAction.swift
@@ -0,0 +1,67 @@
+//
+//  ProcessSuggestedScanRangesAction.swift
+//  
+//
+//  Created by Lukáš Korba on 02.08.2023.
+//
+
+import Foundation
+
+final class ProcessSuggestedScanRangesAction {
+    let rustBackend: ZcashRustBackendWelding
+    let service: LightWalletService
+    let logger: Logger
+    
+    init(container: DIContainer) {
+        service = container.resolve(LightWalletService.self)
+        rustBackend = container.resolve(ZcashRustBackendWelding.self)
+        logger = container.resolve(Logger.self)
+    }
+}
+
+extension ProcessSuggestedScanRangesAction: Action {
+    var removeBlocksCacheWhenFailed: Bool { false }
+
+    func run(with context: ActionContext, didUpdate: @escaping (CompactBlockProcessor.Event) async -> Void) async throws -> ActionContext {
+        logger.info("Getting the suggested scan ranges from the wallet database.")
+        let scanRanges = try await rustBackend.suggestScanRanges()
+        
+        if let firstRange = scanRanges.first {
+            // If there is a range of blocks that needs to be verified, it will always
+            // be returned as the first element of the vector of suggested ranges.
+            if firstRange.priority == .verify {
+                // TODO: [#1189] handle rewind, https://github.com/zcash/ZcashLightClientKit/issues/1189
+                // REWIND to download.start height HERE
+            }
+            
+            let lowerBound = firstRange.range.lowerBound - 1
+            let upperBound = firstRange.range.upperBound - 1
+            
+            let syncControlData = SyncControlData(
+                latestBlockHeight: upperBound,
+                latestScannedHeight: lowerBound,
+                firstUnenhancedHeight: lowerBound + 1
+            )
+            
+            logger.debug("""
+                Init numbers:
+                latestBlockHeight [BC]:         \(upperBound)
+                latestScannedHeight [DB]:       \(lowerBound)
+                firstUnenhancedHeight [DB]:     \(lowerBound + 1)
+                """)
+            
+            await context.update(lastScannedHeight: lowerBound)
+            await context.update(lastDownloadedHeight: lowerBound)
+            await context.update(syncControlData: syncControlData)
+            await context.update(totalProgressRange: lowerBound...upperBound)
+            
+            await context.update(state: .download)
+        } else {
+            await context.update(state: .finished)
+        }
+        
+        return context
+    }
+
+    func stop() async { }
+}
