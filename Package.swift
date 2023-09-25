71a6f580fc8988c121dbee17ad8d2da2aa27ddb0
Kris Nuttycombe <kris@nutty.land>
2023-09-25 15:40:22 -0600

    Update `zcash-light-client-ffi` for 2.0.0 final release.

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/grpc/grpc-swift.git", from: "1.14.0"),
         .package(url: "https://github.com/stephencelis/SQLite.swift.git", from: "0.14.1"),
-        .package(url: "https://github.com/zcash-hackworks/zcash-light-client-ffi", from: "0.4.0-rc.4")
+        .package(url: "https://github.com/zcash-hackworks/zcash-light-client-ffi", from: "0.4.0")
     ],
     targets: [
         .target(
