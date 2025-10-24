53b626ef4337f5af9c8434b9261dcc0011d4e07f
Lukas Korba <lukas.korba@seznam.cz>
2025-10-24 21:49:35 +0200

    Hotfix check of ephemeral addresses

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/grpc/grpc-swift.git", from: "1.24.2"),
         .package(url: "https://github.com/stephencelis/SQLite.swift.git", from: "0.15.3"),
-        .package(url: "https://github.com/Electric-Coin-Company/zcash-light-client-ffi", branch: "preview/feature/single_use_addresses")
+        .package(url: "https://github.com/Electric-Coin-Company/zcash-light-client-ffi", branch: "preview/hotfix/ephemeral_check_ranges")
     ],
     targets: [
         .target(
