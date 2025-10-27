27dc15cb6386113ea96cae46719a7c101c5c5f00
Lukas Korba <lukas.korba@seznam.cz>
2025-10-27 13:26:15 +0100

    SingleUseTransparentAddress cleanup

--- Sources/ZcashLightClientKit/Model/SingleUseTransparentAddress.swift
+++ Sources/ZcashLightClientKit/Model/SingleUseTransparentAddress.swift
@@ -11,55 +11,15 @@ public struct SingleUseTransparentAddress: Equatable {
     let address: String
     let gapPosition: UInt32
     let gapLimit: UInt32
+    
+    public init(address: String, gapPosition: UInt32, gapLimit: UInt32) {
+        self.address = address
+        self.gapPosition = gapPosition
+        self.gapLimit = gapLimit
+    }
 }
 
 public enum SingleUseTransparentResult: Equatable {
     case notFound
     case found(String)
-//    init(from ffi: UnsafeMutablePointer<FfiAddressCheckResult>) {
-//        switch ffi.tag {
-//        case .notFound:
-//            self = .notFound
-//            
-//        case .found:
-//            let address = ffi.found.address.map { String(cString: $0) } ?? ""
-//            self = .found(address: address)
-//        }
-//    }
 }
-
-/*
-typedef struct FfiSingleUseTaddr {
-  char *address;
-  uint32_t gap_position;
-  uint32_t gap_limit;
-} FfiSingleUseTaddr;
-
-
-/**
- * The result of checking for UTXOs received by an ephemeral address.
- *
- */
-enum FfiAddressCheckResult_Tag {
-  /**
-   * No UTXOs were found as a result of the check.
-   */
-  FfiAddressCheckResult_NotFound,
-  /**
-   * UTXOs were found for the given address.
-   */
-  FfiAddressCheckResult_Found,
-};
-typedef uint8_t FfiAddressCheckResult_Tag;
-
-typedef struct FfiAddressCheckResult_Found_Body {
-  char *address;
-} FfiAddressCheckResult_Found_Body;
-
-typedef struct FfiAddressCheckResult {
-  FfiAddressCheckResult_Tag tag;
-  union {
-    FfiAddressCheckResult_Found_Body found;
-  };
-} FfiAddressCheckResult;
-*/
