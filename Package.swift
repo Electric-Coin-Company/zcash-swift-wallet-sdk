7216246645319fe1fdd540c828245a57c1d81c10
Lukas Korba <lukas.korba@seznam.cz>
2025-11-05 14:06:52 +0100

    FFI release candidate 0.19.0

--- Package.swift
+++ Package.swift
@@ -16,12 +16,7 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/grpc/grpc-swift.git", from: "1.24.2"),
         .package(url: "https://github.com/stephencelis/SQLite.swift.git", from: "0.15.3"),
-//        .package(url: "https://github.com/Electric-Coin-Company/zcash-light-client-ffi", revision: "e88a70ae3691f49079f48a439c09e450c4bc5a7e")
-//        .package(url: "https://github.com/Electric-Coin-Company/zcash-light-client-ffi", revision: "884e248c72a3c7e50b0cfac7251d3c48b8509393")
-            .package(url: "https://github.com/Electric-Coin-Company/zcash-light-client-ffi", branch: "preview/fix/relax_ephemeral_address_unspentness_checks")
-        
-//        .package(url: "https://github.com/Electric-Coin-Company/zcash-light-client-ffi", revision: "87e5e1a92767625942368e2cf29ce9610dba3a42")
-        
+        .package(url: "https://github.com/Electric-Coin-Company/zcash-light-client-ffi", branch: "release/0.19.0")
     ],
     targets: [
         .target(
